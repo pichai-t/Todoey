9b5c096dca409818159da088e90eb650fff809c7
Pichai T <pichai@pichais-macbook-pro.local>
2019-03-11 14:00:58 +1100

    clean-up

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = [Item]() // [""]
+    var itemArray = [Item]() // To keep information of cell
     var textField = UITextField()
     
     //var defaults = UserDefaults.standard
+    
+    // Create a Data File Path -- where stores data to physical
     var dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
 
     // 1. WHEN First loading.
@@ -23,46 +25,44 @@ class TodoListViewController: UITableViewController {
         
         print(dataFilePath)
         
-        // Load data from stored data
-        loadData()
+        // Load data from stored data (from "itemArray")
+        loadFromNSCoder2ItemArray()
         
 //        var item = Item()
 //        item.title = "test"
 //        itemArray.append(item)
         
+          /* Load from UserDefault to ItemArray */
 //        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
 //           itemArray = items
 //        }
     
-        
     }
 
-    //MARK - TableView Delegate Methods - 2 required methods
-    // When CHECKING Number of rows
+    //MARK - TableView Delegate Methods - 2 required methods =====
+    // When CHECKING Number of rows - *** First time and whenever 'tableView.reloadData()' called ***
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
-    // When LOADING For Each Cell
+    // When LOADING For Each Cell - *** First time and whenever 'tableView.reloadData()' called ***
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath )
-        
         let item = itemArray[indexPath.row]
         cell.textLabel?.text = item.title
         cell.accessoryType = (item.done == true) ? .checkmark : .none
         
-        return cell
+        return cell  // Then for the first time, tableView.reloadData will be automatically called.
     }
+    // =============================================================
 
     // 2. When SELECT ROW
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print (indexPath.row)
-        
+        // Set 'done' (tick)
         itemArray[indexPath.row].done = !(itemArray[indexPath.row].done)
         
-        saveData()
-        loadData()
+        saveItemArrayToNSCoder()
+        refreshScreen()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -83,8 +83,10 @@ class TodoListViewController: UITableViewController {
             self.itemArray.append(item)
             //self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
-            self.saveData()
-            self.loadData()
+            // Save ItemArray to NSCoder, load from NSCoder and refresh screen.
+            self.saveItemArrayToNSCoder()
+            //self.loadFromNSCoder2ItemArray()
+            self.refreshScreen()
 
         }
         // 3. Add a Text Field to Alert
@@ -101,7 +103,7 @@ class TodoListViewController: UITableViewController {
 
     }
     
-    func saveData() {
+    func saveItemArrayToNSCoder() {
         // Put data into NSCoder
         let encoder = PropertyListEncoder()
         do {
@@ -111,12 +113,14 @@ class TodoListViewController: UITableViewController {
         catch {
             print ("Error encoding item array \(error)")
         }
-        self.tableView.reloadData()
+    }
+    
+    func refreshScreen() {
+        tableView.reloadData()
     }
 
-    func loadData() {
+    func loadFromNSCoder2ItemArray() {
         // Load data from NSCoder
-        
         if let data = try? Data(contentsOf: dataFilePath!) {
             do {
                 let decoder = PropertyListDecoder()
@@ -125,7 +129,6 @@ class TodoListViewController: UITableViewController {
             catch {
                 print("Error in decoding \(error)")
             }
-        
         }
     }
     
